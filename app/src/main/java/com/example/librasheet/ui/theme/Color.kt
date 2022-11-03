a3148b597572385eb8e84396cdd3b17760f28b97
Riley Xu <riley.xu@gmail.com>
2022-11-03 11:37:36 -0400

    Update colors

--- app/src/main/java/com/example/librasheet/ui/theme/Color.kt
+++ app/src/main/java/com/example/librasheet/ui/theme/Color.kt
@@ -1,8 +1,22 @@
 package com.example.librasheet.ui.theme
 
+import androidx.compose.material.darkColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green500 = Color(0xFF1EB969)
+val DarkBlue900 = Color(0xFF26282F)
+
+val CandleGreen = Color(0xFF2DBF78)
+val CandleRed = Color(0xFFEF4747)
+val Black90 = Color(0xFF1A1A1A)
+
+val DarkColorPalette = darkColors(
+    primary = Green500,
+    surface = Black90,
+    onSurface = Color.White,
+    background = Black90,
+    onBackground = Color.White,
+    error = Color(0xFFEB5858),
+)
+
+fun randomColor() = Color((Math.random() * 16777215).toInt() or (0xFF shl 24))
