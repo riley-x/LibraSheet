284542be15f8134c65f6eb07b0f4839301cdf779
Riley Xu <riley.xu@gmail.com>
2022-11-03 23:40:41 -0400

    Add correct padding for status bar.

--- app/src/main/java/com/example/librasheet/ui/LibraApp.kt
+++ app/src/main/java/com/example/librasheet/ui/LibraApp.kt
@@ -1,23 +1,28 @@
 package com.example.librasheet.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.librasheet.Greeting
 import com.example.librasheet.viewModel.LibraViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun LibraApp(
     viewModel: LibraViewModel = viewModel(),
 ) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Text("Hello!", color= Color.White)
+    Scaffold(
+        bottomBar = {},
+        modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)
+    ) { innerPadding ->
+        val bottomPadding = if (WindowInsets.isImeVisible) 0.dp else innerPadding.calculateBottomPadding()
+        Text("Hello!")
     }
 }
