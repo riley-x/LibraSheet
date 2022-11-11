1fe589a83d56e29abc2ef70893623359d7e01425
Riley Xu <riley.xu@gmail.com>
2022-11-11 00:19:58 -0500

    Cleanup

--- app/src/main/java/com/example/librasheet/ui/components/DialSelector.kt
+++ app/src/main/java/com/example/librasheet/ui/components/DialSelector.kt
@@ -1,8 +1,6 @@
 package com.example.librasheet.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -13,23 +11,16 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.example.librasheet.ui.theme.LibraSheetTheme
 import com.example.librasheet.viewModel.dataClasses.ImmutableList
 import com.example.librasheet.viewModel.preview.previewGraphLabels
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -75,18 +66,21 @@ fun DialSelector(
     LaunchedEffect(swipeableState.currentValue) {
         val loop: Boolean
         val index: Int
-        if (swipeableState.currentValue == -1) {
-            loop = true
-            index = labels.items.lastIndex
-            swipeableState.snapTo(index)
-        }
-        else if (swipeableState.currentValue == labels.items.size) {
-            loop = true
-            index = 0
-            swipeableState.snapTo(index)
-        } else {
-            loop = false
-            index = swipeableState.currentValue
+        when (swipeableState.currentValue) {
+            -1 -> {
+                loop = true
+                index = labels.items.lastIndex
+                swipeableState.snapTo(index)
+            }
+            labels.items.size -> {
+                loop = true
+                index = 0
+                swipeableState.snapTo(index)
+            }
+            else -> {
+                loop = false
+                index = swipeableState.currentValue
+            }
         }
         onSelection(index, loop)
     }
@@ -185,7 +179,6 @@ fun DialSelector(
 @Preview
 @Composable
 private fun Preview() {
-    val selected = remember { mutableStateOf(0) }
     LibraSheetTheme {
         Surface {
             DialSelector(labels = previewGraphLabels)
