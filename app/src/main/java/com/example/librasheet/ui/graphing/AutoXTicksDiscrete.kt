34d3dafd9dc7a6afd3d5e17684b4350eb9ebc193
Riley Xu <riley.xu@gmail.com>
2022-11-28 16:46:50 -0500

    Spread out x ticks again

--- app/src/main/java/com/example/librasheet/ui/graphing/AutoXTicksDiscrete.kt
+++ app/src/main/java/com/example/librasheet/ui/graphing/AutoXTicksDiscrete.kt
@@ -3,20 +3,26 @@ package com.example.librasheet.ui.graphing
 import com.example.librasheet.viewModel.dataClasses.NamedValue
 import kotlin.math.roundToInt
 
+/**
+ * Creates equally-spaced x ticks for a discrete x axis with [size] points.
+ *
+ * @param ticks is the approximate number of ticks to display. The exact value is not guaranteed.
+ */
 fun autoXTicksDiscrete(size: Int, ticks: Int, labelFormat: (Int) -> String): List<NamedValue> {
     fun toNamedValue(it: Int) = NamedValue(
         value = it.toFloat(),
         name = labelFormat(it)
     )
-    val stepX = (size.toFloat() / (ticks + 1)).roundToInt()
+    val stepX = ((size - 1).toFloat() / ticks).roundToInt()
 
     /** Edge case when few data points **/
     if (stepX <= 1) return List(size, ::toNamedValue)
 
     val indices = mutableListOf<Int>()
-    for (i in 1..ticks) { // pick the center values (dropping 0 and ticks + 1)
-        val pos = stepX * i
-        if (pos >= 0 && pos < size) indices.add(pos)
+    var pos = stepX / 2
+    while (pos < size) {
+        indices.add(pos)
+        pos += stepX
     }
     return indices.map(::toNamedValue)
 }
